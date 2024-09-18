7523029534c9b17292331d6bf155dc33f911dbbd
fayinana <fekedeananiya@gmail.com>
2024-09-18 20:19:21 +0300

    add cors for frontend access

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
 const userRouter = require('./routes/userRoutes');
 const quizRouter = require('./routes/quizRoutes');
 const questionAnswerRouter = require('./routes/questionAnswerRoutes');
@@ -6,6 +8,13 @@ const globalErrorHandler = require('./controllers/errorController');
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
+const corsOptions = {
+  origin: ['http://localhost:3000'],
+  credentials: true
+};
+app.use(cors(corsOptions));
+
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/quizzes', quizRouter);
 app.use('/api/v1/qna', questionAnswerRouter);
