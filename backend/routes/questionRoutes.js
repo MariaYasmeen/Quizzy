ed17bcb3d6c512aa274a21515a681e5db586d832
fayinana <fekedeananiya@gmail.com>
2024-09-25 13:15:47 +0300

    add authorization for question route

--- backend/routes/questionRoutes.js
+++ backend/routes/questionRoutes.js
@@ -3,16 +3,24 @@
 
 const express = require('express');
 const questionController = require('./../controllers/questionController');
+const authController = require('./../controllers/authController');
 
 const router = express.Router({ mergeParams: true });
-
+router.use(authController.protect);
 router
   .route('/')
   .get(questionController.getAllQuestion)
   .post(questionController.createQuestion);
+
 router
   .route('/:id')
   .get(questionController.getQuestion)
-  .patch(questionController.updateQuestion)
-  .delete(questionController.deleteQuestion);
+  .patch(
+    authController.restrictTo('teacher', 'admin'),
+    questionController.updateQuestion
+  )
+  .delete(
+    authController.restrictTo('teacher', 'admin'),
+    questionController.deleteQuestion
+  );
 module.exports = router;
