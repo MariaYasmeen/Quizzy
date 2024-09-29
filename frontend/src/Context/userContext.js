81bc192b0b9ead226db982df334200177b34f9ed
fayinana <fekedeananiya@gmail.com>
2024-09-29 08:07:24 +0300

    creating quiz and question

--- frontend/src/Context/userContext.js
+++ frontend/src/Context/userContext.js
@@ -1,23 +1,25 @@
 // src/contexts/UserContext.js
-import React, { createContext, useEffect, useState } from 'react';
+import { createContext, useEffect, useState } from "react";
 
 export const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
   const [user, setUser] = useState(() => {
-    const storedUser = localStorage.getItem('user');
-    return storedUser && storedUser !== 'undefined' ? JSON.parse(storedUser) : null;
+    const storedUser = localStorage.getItem("user");
+    return storedUser && storedUser !== "undefined"
+      ? JSON.parse(storedUser)
+      : null;
   });
 
   const saveUser = (userData) => {
     setUser(userData);
-    localStorage.setItem('user', JSON.stringify(userData));
+    localStorage.setItem("user", JSON.stringify(userData));
   };
 
   const logoutUser = () => {
     setUser(null);
-    localStorage.removeItem('authToken');
-    localStorage.removeItem('user');
+    localStorage.removeItem("authToken");
+    localStorage.removeItem("user");
   };
 
   useEffect(() => {
--- frontend/src/services/apiQuestion.js
+++ frontend/src/services/apiQuestion.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 const URL = "http://localhost:3300/api/v1/quizzes";
-export async function createQuiz(data) {
+export async function createQuestion({ data, quizId }) {
   try {
-    const res = await axios.post(URL, data);
-    console.log(res.data);
+    const res = await axios.post(`${URL}/${quizId}/questions`, data);
     return res.data.data;
   } catch (error) {
     throw error.data;
